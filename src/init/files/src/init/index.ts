28c6bf89e34809a76cffbbc8f0a09eb880f57f47
tgorka <tomasz.gorka@carmoov.io>
2018-09-11 14:14:00 +0200

    unifiy " and '

--- src/init/files/src/init/index.ts
+++ src/init/files/src/init/index.ts
@@ -1,13 +1,13 @@
-import { strings } from "@angular-devkit/core";
+import { strings } from '@angular-devkit/core';
 import {
     Rule,
     apply,
     mergeWith,
     template,
     url,
-} from "@angular-devkit/schematics";
+} from '@angular-devkit/schematics';
 import { dasherize, classify } from '@angular-devkit/core/src/utils/strings';
-import { Schema as Options } from "./schema";
+import { Schema as Options } from './schema';
 
 
 const stringUtils = { dasherize, classify };
@@ -15,12 +15,12 @@ const stringUtils = { dasherize, classify };
 // You don't have to export the function as default. You can also have more than one rule factory
 // per file.
 export function init(options: Options): Rule {
-    return mergeWith(apply(url("./files"), [
+    return mergeWith(apply(url('./files'), [
         template({
             utils: strings,
             ...stringUtils,
             ...options,
-            "dot": ".",
+            'dot': '.',
             //latestVersions,
         }),
     ]));
--- src/init/index.ts
+++ src/init/index.ts
@@ -5,16 +5,16 @@
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://github.com/tgorka/cliberry/LICENSE
  */
-import { strings } from "@angular-devkit/core";
+import { strings } from '@angular-devkit/core';
 import {
     Rule,
     apply,
     mergeWith,
     template,
     url,
-} from "@angular-devkit/schematics";
+} from '@angular-devkit/schematics';
 import { dasherize, classify } from '@angular-devkit/core/src/utils/strings';
-import { Schema as Options } from "./schema";
+import { Schema as Options } from './schema';
 
 
 const stringUtils = { dasherize, classify };
@@ -22,12 +22,12 @@ const stringUtils = { dasherize, classify };
 // You don't have to export the function as default. You can also have more than one rule factory
 // per file.
 export function init(options: Options): Rule {
-    return mergeWith(apply(url("./files"), [
+    return mergeWith(apply(url('./files'), [
         template({
             utils: strings,
             ...stringUtils,
             ...options,
-            "dot": ".",
+            'dot': '.',
             //latestVersions,
         }),
     ]));
